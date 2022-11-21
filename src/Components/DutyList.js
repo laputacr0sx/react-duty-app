7b7ca8f3aadeb0c3b2dc509ae39f9f0e1b9f34b4
Veilics <>
2022-11-21 17:15:24 +0800

    Implemented - tailwindcss auto sort

--- src/Components/DutyList.js
+++ src/Components/DutyList.js
@@ -1,65 +1,64 @@
-import AddIcon from '@mui/icons-material/Add';
-import RemoveIcon from '@mui/icons-material/Remove';
-import { IconButton } from '@mui/material';
-import moment from 'moment';
-import React, { useEffect, useState } from 'react';
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import { IconButton } from "@mui/material";
+import moment from "moment";
+import React, { useEffect, useState } from "react";
 
-import WeekDate from './WeekDate';
+import WeekDate from "./WeekDate";
 
 const currentWeekNumber = moment().isoWeeks();
 
 export default function DutyList() {
-	const [weekNumber, setWeekNumber] = useState(currentWeekNumber);
+  const [weekNumber, setWeekNumber] = useState(currentWeekNumber);
 
-	const correspondingWeek = (correspondingWeekNumber) => {
-		const startOfWeek = moment()
-			.isoWeeks(correspondingWeekNumber)
-			.startOf('isoWeeks');
+  const correspondingWeek = (correspondingWeekNumber) => {
+    const startOfWeek = moment()
+      .isoWeeks(correspondingWeekNumber)
+      .startOf("isoWeeks");
 
-		const endOfWeek = moment()
-			.isoWeeks(correspondingWeekNumber)
-			.endOf('isoWeeks');
+    const endOfWeek = moment()
+      .isoWeeks(correspondingWeekNumber)
+      .endOf("isoWeeks");
 
-		let days = [];
-		let day = startOfWeek;
-		while (day <= endOfWeek) {
-			days.push(day.toDate());
-			day = day.clone().add(1, 'd');
-		}
+    let days = [];
+    let day = startOfWeek;
+    while (day <= endOfWeek) {
+      days.push(day.toDate());
+      day = day.clone().add(1, "d");
+    }
+    return days;
+  };
 
-		return days;
-	};
+  function increment() {
+    setWeekNumber(weekNumber + 1);
+  }
 
-	function increment() {
-		setWeekNumber(weekNumber + 1);
-	}
+  function decrement() {
+    if (weekNumber === 1) return;
+    setWeekNumber(weekNumber - 1);
+  }
 
-	function decrement() {
-		if (weekNumber === 1) return;
-		setWeekNumber(weekNumber - 1);
-	}
+  useEffect(() => {
+    correspondingWeek(weekNumber);
+  }, [weekNumber]);
 
-	useEffect(() => {
-		correspondingWeek(weekNumber);
-	}, [weekNumber]);
-
-	return (
-		<div className='flex-col flex bg-gray-900 h-screen'>
-			<div className='m-2 flex justify-between'>
-				<h1 className={'text-red-700'}>Duty</h1>
-				<div className='flex justify-between items-center'>
-					<IconButton size={'large'} onClick={decrement}>
-						<RemoveIcon fontSize={'inherit'} color={'primary'} />
-					</IconButton>
-					<h2 className={'text-amber-100'}>{weekNumber}</h2>
-					<IconButton size={'large'} onClick={increment}>
-						<AddIcon fontSize={'inherit'} color={'primary'} />
-					</IconButton>
-				</div>
-			</div>
-			<div>
-				<WeekDate correspondingWeekDates={correspondingWeek(weekNumber)} />
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex h-screen flex-col bg-gray-900">
+      <div className="m-2 flex justify-between">
+        <h1 className="text-red-700">Duty</h1>
+        <div className="flex items-center justify-between">
+          <IconButton size={"large"} onClick={decrement}>
+            <RemoveIcon fontSize={"inherit"} color={"primary"} />
+          </IconButton>
+          <h2 className={"text-amber-100"}>{weekNumber}</h2>
+          <IconButton size={"large"} onClick={increment}>
+            <AddIcon fontSize={"inherit"} color={"primary"} />
+          </IconButton>
+        </div>
+      </div>
+      <div>
+        <WeekDate correspondingWeekDates={correspondingWeek(weekNumber)} />
+      </div>
+    </div>
+  );
 }
--- src/Components/WeekDate.js
+++ src/Components/WeekDate.js
@@ -1,40 +1,41 @@
-import moment from 'moment';
+import moment from "moment";
 
 export default function WeekDate({ correspondingWeekDates }) {
-	const clickHandler = () => {
-		console.log(`element clicked`);
-	};
+  const clickHandler = () => {
+    console.log(`element clicked`);
+  };
 
-	function handlerInput() {
-		console.log(`element clicked`);
-	}
+  function handlerInput() {
+    console.log(`element clicked`);
+  }
 
-	return (
-		<div>
-			{correspondingWeekDates.map((date) => {
-				return (
-					<div
-						className={
-							'flex p-3 px-5 m-2 justify-between align-center text-left bg-blue-400 text-gray-900 rounded-2xl'
-						}
-						onClick={clickHandler}
-						key={date}>
-						<div className={'font-mono'}>
-							{moment(date, 'YYYYMMDD ddd').format('DD/MM ddd')}
-						</div>
-						<div className={'font-sans'}>{'U15'}</div>
-						<input
-							type={'text'}
-							onChange={handlerInput}
-							className={
-								'font-mono bg-transparent w-14 p-1 border-2 rounded placeholder-amber-50'
-							}
-							placeholder={'101'}
-							value={''}
-						/>
-					</div>
-				);
-			})}
-		</div>
-	);
+  return (
+    <div>
+      {correspondingWeekDates.map((date) => {
+        return (
+          <div
+            className={
+              "align-center m-2 flex justify-between rounded-2xl bg-blue-400 p-3 px-5 text-left text-gray-900"
+            }
+            onClick={clickHandler}
+            key={date}
+          >
+            <div className={"font-mono"}>
+              {moment(date, "YYYYMMDD ddd").format("DD/MM ddd")}
+            </div>
+            <div className={"font-sans"}>{"U15"}</div>
+            <input
+              type={"text"}
+              onChange={handlerInput}
+              className={
+                "w-14 rounded border-2 bg-transparent p-1 font-mono placeholder-amber-50"
+              }
+              placeholder={"101"}
+              value={""}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
 }
