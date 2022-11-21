daf83cdf2e36865c0ffc7c7d0930c150ba425e11
Veilics <>
2022-11-21 16:56:26 +0800

    webStorm initiate

--- src/Components/DutyList.js
+++ src/Components/DutyList.js
@@ -1,63 +1,38 @@
+import AddIcon from "@mui/icons-material/Add";
+import { IconButton } from "@mui/material";
 import moment from "moment";
 import React, { useEffect, useState } from "react";
-import { IconButton } from "@mui/material";
-import AddIcon from "@mui/icons-material/Add";
-import RemoveIcon from "@mui/icons-material/Remove";
 
-import WeekDate from "./WeekDate";
-
-import "./DutyList.css";
-
-const currentWeekNumber = moment().day("Monday").isoWeeks();
+const currentWeekNumber = moment().isoWeeks();
 
 export default function DutyList() {
-  const [weekNumber, setWeekNumber] = useState(currentWeekNumber);
+  const [weekNumber, setWeekNumber] = useState(0);
 
-  const correspondingWeek = (correspondingWeekNumber) => {
-    const Monday = moment()
-      .isoWeeks(correspondingWeekNumber)
-      .startOf("isoWeeks");
-
-    let dates = [];
-    let day = Monday.format("YYYYMMDD ddd");
-    for (let i = 0; i < 7; i++) {
-      dates.push(day);
-      day = Monday.add(1, "days").format("YYYYMMDD ddd");
-    }
-
-    return dates;
-  };
-
-  useEffect(() => {
-    correspondingWeek(weekNumber);
-  }, [weekNumber]);
-
-  const increment = () => {
+  function clickHandler() {
     setWeekNumber(weekNumber + 1);
-  };
+  }
 
   const decrement = () => {
     if (weekNumber === 1) return;
     setWeekNumber(weekNumber - 1);
   };
 
+  useEffect(() => {
+    correspondingWeek(weekNumber);
+  }, [weekNumber]);
+
   return (
-    <div className={"appContainer"}>
-      <div className={"appHeading"}>
-        <h1>Duty</h1>
-        <div className={"weekController"}>
-          <IconButton size={"large"} onClick={decrement}>
-            <RemoveIcon fontSize={"inherit"} color={"primary"} />
-          </IconButton>
-          <h2>{weekNumber}</h2>
-          <IconButton size={"large"} onClick={increment}>
-            <AddIcon fontSize={"inherit"} color={"primary"} />
-          </IconButton>
+    <>
+      <div className={"appContainer"}>
+        <h1>This is the List View</h1>
+        <p>{weekNumber}</p>
+        <IconButton size={"large"} onClick={clickHandler}>
+          <AddIcon fontSize={"inherit"} />
+        </IconButton>
+        <div>
+          <WeekDate weekNumber={weekNumber} />
         </div>
       </div>
-      <div>
-        <WeekDate correspondingWeekDates={correspondingWeek(weekNumber)} />
-      </div>
-    </div>
+    </>
   );
 }
--- src/Components/WeekDate.js
+++ src/Components/WeekDate.js
@@ -1,23 +1,34 @@
-import moment from "moment";
-
-import { WEEKDATE } from "../data/DUMMY";
-
-import "./WeekDate.css";
+import moment from 'moment';
 
 export default function WeekDate({ correspondingWeekDates }) {
-  return (
-    <div>
-      {correspondingWeekDates.map((date) => {
-        return (
-          <div className={"weekDate"} key={date}>
-            <div className={"date"}>
-              {moment(date, "YYYYMMDD ddd").format("DD/MM ddd")}
-            </div>
-            <div className={"dutyPrefix"}>{"U15"}</div>
-            <div className={"dutyNumber"}>{"dutyNumber"}</div>
-          </div>
-        );
-      })}
-    </div>
-  );
+	function handlerInput() {}
+
+	return (
+		<div>
+			{correspondingWeekDates.map((date) => {
+				return (
+					<div
+						className={
+							'flex p-3 px-5 m-2 justify-between text-left bg-blue-400 text-gray-900 rounded-2xl'
+						}
+						onClick={handlerInput}
+						key={date}>
+						<div className={'font-mono'}>
+							{moment(date, 'YYYYMMDD ddd').format('DD/MM ddd')}
+						</div>
+						<div className={'font-sans'}>{'U15'}</div>
+						<input
+							type={'text'}
+							onChange={handlerInput}
+							className={
+								'font-mono bg-transparent w-14 p-1 border-2 rounded placeholder-amber-50'
+							}
+							placeholder={'101'}
+							value={''}
+						/>
+					</div>
+				);
+			})}
+		</div>
+	);
 }
